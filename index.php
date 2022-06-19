84a06790fc2759d57db5dbad224c4fecb25ead21
MosTafaHosamm <mostafa.hosam1996@gmail.com>
2022-06-19 11:34:32 +0200

    insert method Two

--- index.php
+++ index.php
@@ -1,15 +1,33 @@
 <?php
 include "connect.php";
 
+// $stmt=$con->prepare(
+// "INSERT INTO `users` (`name`,`email`)
+//  VALUES
+//   ('hosam','HosamEldinMohamed@gmail.com'),
+//   ('Eman','EmanHola@gmail.com')
+//
+//   ");
+
+// $stmt=$con->prepare(
+// "INSERT INTO `users` (`name`,`email`)
+//  VALUES
+//   (?,?) ");
+//    $stmt->execute(
+//    array( "Qamar", "Qamar@gmail.com" ) );
+
+
+// using placeholder sign (:)
+
 $stmt=$con->prepare(
 "INSERT INTO `users` (`name`,`email`)
  VALUES
-  ('hosam','HosamEldinMohamed@gmail.com'),
-  ('Eman','EmanHola@gmail.com')
+  (:n,:e) ");
+   $stmt->execute(
+  array(':n'=>'Wfaa',':e'=>'wfaa@gmail.com') );
 
-  ");
-$stmt->execute();
-$count=$stmt->rowCount();
+
+ $count=$stmt->rowCount();
 if($count>0)
 {
 echo"User Added Successfully";
