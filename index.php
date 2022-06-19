ca78cca273d2a9391824e2deb4a19e310d7a7235
MosTafaHosamm <mostafa.hosam1996@gmail.com>
2022-06-19 18:13:02 +0200

    signUp

--- index.php
+++ index.php
@@ -9,10 +9,14 @@ include 'connect.php';
 // );
 
 
-$stmt=$con->prepare("UPDATE users SET name=?, email=? WHERE id=?");
-$stmt->execute(
-array('KHoloud','kholod@gmail.com',1)
-);
+// $stmt=$con->prepare("UPDATE users SET name=?, email=? WHERE id=?");
+// $stmt->execute(
+// array('KHoloud','kholod@gmail.com',1)
+// );
+
+$stmt=$con->prepare("DELETE FROM `users` WHERE id=:i");
+$stmt->execute(array(':i'=>8));
+
 $count=$stmt->rowCount();
 if($count){
 echo"Success";
