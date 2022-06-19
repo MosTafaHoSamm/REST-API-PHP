32ddb21f688d035e2b71fe017faf24dd2710db78
MosTafaHosamm <mostafa.hosam1996@gmail.com>
2022-06-19 12:06:57 +0200

    Update method

--- index.php
+++ index.php
@@ -1,38 +1,23 @@
 <?php
-include "connect.php";
-
-// $stmt=$con->prepare(
-// "INSERT INTO `users` (`name`,`email`)
-//  VALUES
-//   ('hosam','HosamEldinMohamed@gmail.com'),
-//   ('Eman','EmanHola@gmail.com')
-//
-//   ");
-
-// $stmt=$con->prepare(
-// "INSERT INTO `users` (`name`,`email`)
-//  VALUES
-//   (?,?) ");
-//    $stmt->execute(
-//    array( "Qamar", "Qamar@gmail.com" ) );
+include 'connect.php';
+// $stmt=$con->prepare("UPDATE users SET name=:n, email=:e WHERE id=:i");
+// $stmt->execute(
+// array(':n'=>'Hasnaa',
+// ':e'=>'Hasnaa@gmail.com',
+// ':i'=>2
+// )
+// );
 
 
-// using placeholder sign (:)
-
-$stmt=$con->prepare(
-"INSERT INTO `users` (`name`,`email`)
- VALUES
-  (:n,:e) ");
-   $stmt->execute(
-  array(':n'=>'Wfaa',':e'=>'wfaa@gmail.com') );
-
-
- $count=$stmt->rowCount();
-if($count>0)
-{
-echo"User Added Successfully";
+$stmt=$con->prepare("UPDATE users SET name=?, email=? WHERE id=?");
+$stmt->execute(
+array('KHoloud','kholod@gmail.com',1)
+);
+$count=$stmt->rowCount();
+if($count){
+echo"Success";
 }
 else{
-echo"Failed Adding User";
+echo"fail";
 }
 ?>
