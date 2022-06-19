a1d97eaf0addef6ecd5ebea6bb0f2c83df3130e0
MosTafaHosamm <mostafa.hosam1996@gmail.com>
2022-06-19 11:25:01 +0200

    insert method One

--- index.php
+++ index.php
@@ -1,28 +1,20 @@
 <?php
 include "connect.php";
-$stmt=$con->prepare('SELECT name FROM users WHERE id=1 ');
+
+$stmt=$con->prepare(
+"INSERT INTO `users` (`name`,`email`)
+ VALUES
+  ('hosam','HosamEldinMohamed@gmail.com'),
+  ('Eman','EmanHola@gmail.com')
+
+  ");
 $stmt->execute();
-$users=$stmt->fetchAll( );
 $count=$stmt->rowCount();
-
-echo"<pre>";
-echo"$count";
-echo"<br/>";
-
-echo json_encode(
-array(
-array(
-"name"=>"Mostafa",
-"email"=>"Mostafa@gmail.com",
-"phone"=>"01017242252"),
-array(
-"name"=>"Amer",
-"email"=>"Amer@gmail.com",
-"phone"=>"0101725624364"),
-array(
-"name"=>"Asmaa",
-"email"=>"asmaa@gmail.com",
-"phone"=>"012283833")
-));
-echo "</pre>";
+if($count>0)
+{
+echo"User Added Successfully";
+}
+else{
+echo"Failed Adding User";
+}
 ?>
